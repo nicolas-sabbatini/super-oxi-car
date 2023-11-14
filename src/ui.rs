1328e6231f2cd9c588c91d238d9aa80b1b1c8ea8
Nicolas Sabbatini <nik.code.things@gmail.com>
2023-11-13 22:23:33 -0300

    Fonts

--- src/ui.rs
+++ src/ui.rs
@@ -3,7 +3,7 @@ use bevy::{
     text::{BreakLineOn, Text2dBounds},
 };
 
-use crate::config::{P8_CREAM, WINDOW_HEIGHT, WINDOW_WIDTH};
+use crate::config::{P8_CREAM, P8_GREY_DARK, WINDOW_HEIGHT, WINDOW_WIDTH};
 
 pub const TOP_BAR_HEIGHT: f32 = 64.0;
 const TOP_BAR_WIDTH: f32 = WINDOW_WIDTH;
@@ -19,6 +19,13 @@ impl Plugin for Plug {
 }
 
 fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("ArchivoBlack-Regular.ttf");
+    let text_style = TextStyle {
+        font,
+        font_size: 18.0,
+        color: P8_GREY_DARK,
+    };
+
     commands.spawn(SpriteBundle {
         sprite: Sprite {
             color: P8_CREAM,
@@ -32,9 +39,10 @@ fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
         )),
         ..default()
     });
+
     commands.spawn(Text2dBundle {
         text: Text {
-            sections: vec![TextSection::new("Player 1 Score: 0", TextStyle::default())],
+            sections: vec![TextSection::new("Player 1 Score: 1000", text_style)],
             alignment: TextAlignment::Left,
             linebreak_behavior: BreakLineOn::AnyCharacter,
         },
